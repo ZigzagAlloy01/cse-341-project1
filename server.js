c754a740427b7ec1bf136577469ab142e32b6e51
ZigzagAlloy01 <ivanblued@gmail.com>
2024-09-23 07:43:52 -0600

    Mongo db

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongodb = require('./routes/data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is running on port ${port}`)
+        })
+    }
+});
 
 app.listen(port, () => {console.log(`Running on port ${port}`)});
